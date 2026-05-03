760da59d0d7ae6c3344e86208a51477f03aa8504
ByteMe6 <tsapiy.yurka@gmail.com>
2026-05-03 11:48:46 +0300

    alias for ssh connection via xterm
    without xterm in kitty not renders tui, such as nano or vim, colors not working too

--- fish/config.fish
+++ fish/config.fish
@@ -3,42 +3,33 @@ if status is-interactive
     set -g fish_greeting
 
     # Aliases
-    alias fastBtw="fastfetch --logo ~/pics/archLogo.png --logo-type kitty"
-    
-    # to install if i need  alias soundSettings="GDK_BACKEND=x11 easyeffects"
-
     alias sound="GDK_BACKEND=x11 pavucontrol"
-    alias record='wf-recorder -f /tmp/tmp.mkv && ffmpeg -y -i /tmp/tmp.mkv -c:v libx264 ~/Videos/recording_$(date +%Y%m%d_%H%M%S).mp4 && rm /tmp/tmp.mkv'
+    alias record='wf-recorder -f /tmp/tmp.mkv && ffmpeg -y -i /tmp/tmp.mkv -c:v libx264 ~/Videos/recording_(date +%Y%m%d_%H%M%S).mp4 && rm /tmp/tmp.mkv'
     alias f="fastfetch"
     alias fishreload="source ~/.config/fish/config.fish"
     alias clock="tty-clock -c -C7 -s"
-    # alias update="sudo pacman -Syu"
     alias cls="clear"
     alias ll="ls -lh --color=auto"
-    
-
-    # Поднять Warp
-    alias warp-up="sudo wg-quick up ~/wgcf-profile.conf"
-
-    # Опустить Warp
-    alias warp-down="sudo wg-quick down ~/wgcf-profile.conf"
-
     alias ..="cd .."
     alias ...="cd ../.."
     alias ....="cd ../../.."
     alias home="cd ~"
     alias docs="cd ~/documents"
     alias dl="cd ~/downloads"
-
     alias gs="git status"
-    alias ga="git add"
+    alias ga="git1 add"
     alias gc="git commit -m"
     alias gp="git push"
-
     alias please="sudo"
     alias lelouch_britanskiy_povelevayet="sudo"
     alias pls="sudo"
-    # fastfetch
-    fastfetch 
+    alias ssh="TERM=xterm-256color command ssh"
+
+    # fastfetch в зависимости от терминала
+    if set -q KITTY_WINDOW_ID
+        fastfetch --logo ~/pics/torfinFaceFastfetch.png --logo-type kitty
+    else
+        # Маленький PNG, либо уменьшенный прямой вывод
+        fastfetch
+    end
 end
-    # fastfetch --logo /home/maksym/pictures/ff-logo.png --logo-type kitty-direct --logo-width 20 --logo-height 10
